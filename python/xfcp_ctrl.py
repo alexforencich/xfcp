38d7a4913b3011190646923a969f115d8c07b3ca
Alex Forencich <alex@alexforencich.com>
2019-08-01 16:15:54 -0700

    Update xfcp_ctrl

--- python/xfcp_ctrl.py
+++ python/xfcp_ctrl.py
@@ -64,7 +64,7 @@ def main():
 
     if do_enumerate:
         do_enumerate = False
-        print('\n'.join(format_tree(n)))
+        n.print_tree()
     else:
         do_enumerate = True
 
@@ -148,32 +148,7 @@ def main():
                 print("Error: not a I2CNode (%s)" % path)
 
     if do_enumerate:
-        print('\n'.join(format_tree(n)))
-
-
-def node_string(node):
-    s = ''
-    if len(node.path) > 0: s += '[%s] ' % '.'.join(str(x) for x in node.path)
-    s += type(node).__name__
-    s += ' [%s]' % node.name
-    if len(node.ext_str) > 0:
-        s += ' [%s]' % node.ext_str
-    return s
-
-def format_tree(node):
-    s = node_string(node)
-    lst = [s]
-    for ni in range(len(node)):
-        lst2 = format_tree(node[ni])
-        for i in range(len(lst2)):
-            if i == 0:
-                lst.append(' |__'+lst2[i])
-            else:
-                if ni == len(node)-1:
-                    lst.append('    '+lst2[i])
-                else:
-                    lst.append(' |  '+lst2[i])
-    return lst
+        n.print_tree()
 
 if __name__ == "__main__":
     main()
