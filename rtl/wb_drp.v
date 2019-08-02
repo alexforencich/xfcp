92989bd0f38f45bc4b9a3cb6a22828038ea79844
Alex Forencich <alex@alexforencich.com>
2019-08-02 02:08:51 -0700

    Fix use before define

--- rtl/wb_drp.v
+++ rtl/wb_drp.v
@@ -59,14 +59,14 @@ module wb_drp #
     input  wire                    drp_rdy
 );
 
+reg cycle = 1'b0;
+
 assign drp_addr = wb_adr_i;
 assign drp_do = wb_dat_i;
 assign wb_dat_o = drp_di;
 assign drp_en = wb_cyc_i & wb_stb_i & ~cycle;
 assign drp_we = wb_cyc_i & wb_stb_i & wb_we_i & ~cycle;
 assign wb_ack_o = drp_rdy;
-
-reg cycle = 1'b0;
 
 always @(posedge clk) begin
     if (rst) begin
